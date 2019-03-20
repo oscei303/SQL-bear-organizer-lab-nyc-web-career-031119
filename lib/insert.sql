fb0b772a348683cd67b92643e9c9c20ac2ea8fa2
OSCAR R <oscr.rss@gmail.com>
2019-03-20 22:48:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'male', 'Black', 'aggressive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'Brown', 'sad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'female', 'Black/Brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'male', 'Black', 'excited', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'female', 'Light Brown', 'spontaneous', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 10, 'male', 'Green', 'dissappointed', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 1, 'female', 'Black', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 99, 'male', 'Red', 'spiritual', 1);
